03a052c72c566d80c79fcebeb85e1700e26c363c
Seb Bacon <seb.bacon@gmail.com>
2017-08-07 21:58:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+To publish an article:
+
+   Add a file to `content/`
+
+To get started:
+
+   pip install -r requirements.txt
+
+To generate file:
+
+   pelican content
+
+To publish:
+
+    cp -R content/ ../stroudinvestigates.github.io/
+    cd ../stroudinvestigates.github.io
+    git push   
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Stroud Investigates Volunteers'
+SITENAME = u'Stroud Investigates'
+SITEURL = ''
+THEME = '/home/seb/Code/pelican-themes/pelican-bootstrap3/'
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+BOOTSTRAP_THEME = 'simplex'
+
+PATH = 'content'
+
+PLUGIN_PATHS = ['/home/seb/Code/pelican-plugins']
+PLUGINS = ['i18n_subsites']
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
